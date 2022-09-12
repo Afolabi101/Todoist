6bd781dfa15c42193fbfe0bcfe0ec984a85fd607
Bakare Afolabi <bakareoafolabi@gmail.com>
2022-09-12 16:35:44 +0100

    Deploy React app

---
+++ build/precache-manifest.52b98fb374e444c1d0dd6ce5ff846343.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b115c9341b235bf0c884218272e0de04",
+    "url": "/Todoist/index.html"
+  },
+  {
+    "revision": "22fc805818f7be5c7425",
+    "url": "/Todoist/static/js/2.60dc8bf2.chunk.js"
+  },
+  {
+    "revision": "002c1cfd1fd6aae055c0",
+    "url": "/Todoist/static/js/main.96630b7a.chunk.js"
+  },
+  {
+    "revision": "ebaba0367fbb5042626e",
+    "url": "/Todoist/static/js/runtime-main.f593a84f.js"
+  }
+]);
